e1ba7b70524ef71a5e8b1c85b18b98dce1478f0e
Itaakara Robertson <Itaanikus@gmail.com>
2020-03-03 23:10:01 +0100

    Fix members

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const leagueGameTypeId = 2;
+
 function myOpGgFunction() {
     var inputElement = document.getElementById("teamIdInput");
     var inputValue = +inputElement.value
@@ -9,21 +11,38 @@ function myOpGgFunction() {
         return;
     }
 
-    getTeamInfo(inputValue);
-};
+    getTeamInfo(inputValue).then(teamMembers => {
+        console.log(teamMembers)
+        teamMembers.forEach(member => {
+            console.log(member);
+        });
+    });
+}
 
 function getTeamInfo(teamId) {
-    var teamUrl = "https://app.esportligaen.dk/api/team/" + teamId + "?includeViewInfo=true";
+    const teamUrl = "https://app.esportligaen.dk/api/team/" + teamId + "?includeViewInfo=true";
 
     return fetchUrl(teamUrl).then(response => {
-        // Members from json object..
         var teamMembers = [];        
         response.members.forEach(member => {
-            teamMembers.push( { id: member.id, nickname: member.nickname } )
+            var nickName = member.user.nickname;
+            
+            const userUrl = "https://app.esportligaen.dk/api/user/" + member.user.id + "?includeGameTeamInfo=true"
+            fetchUrl(userUrl).then(response => {
+                var gamerIds = [];
+                response.gameLogins.filter(login => login.gameLoginTypeId === leagueGameTypeId).forEach(login => {
+                    gamerIds.push(login.gamerId)
+                });
+
+                gamerIds.length > 0 ? 
+                Array.prototype.push.apply(teamMembers, gamerIds)
+                : teamMembers.push(nickName);
+            });            
         });
-        
+
+        return teamMembers;
     });
-};
+}
 
 function fetchUrl(url) {
     console.log("Fetching url: " + url);
